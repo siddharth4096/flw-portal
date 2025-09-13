7ce75650e957e58ac0a2648a505fa3181096b070
Siddharth4096 <siddarthkashyap46@gmail.com>
2025-09-14 01:57:06 +0530

    Delete welcome-pages directory

--- welcome-pages/loader.js
+++
@@ -1,7 +0,0 @@
-// Hide loader once page fully loads
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  const content = document.getElementById("content");
-  if (loader) loader.style.display = "none";
-  if (content) content.style.display = "block";
-});
